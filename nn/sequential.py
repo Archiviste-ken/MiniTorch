962083f4cf7aa00c86c8c7640dc5132cee529b94
Shreyesh <shreyeshpachpor06@gmail.com>
2026-08-12 04:24:41 +0530

    Refactor Sequential class to simplify initialization and forward method

--- nn/sequential.py
+++ nn/sequential.py
@@ -1,19 +1,14 @@
-from __future__ import annotations
-
-from typing import Iterable
-
-from .module import Module
+from nn.module import Module
 
 
 class Sequential(Module):
-    def __init__(self, *modules: Module) -> None:
-        self.layers = list(modules)
 
-    def parameters(self) -> list[object]:
-        parameters: list[object] = []
-        for module in self.layers:
-            parameters.extend(module.parameters())
-        return parameters
+    def __init__(self, *modules):
+        self.modules = list(modules)
 
-    def modules(self) -> Iterable[Module]:
-        return list(self.layers)
+    def forward(self, x):
+
+        for module in self.modules:
+            x = module(x)
+
+        return x
